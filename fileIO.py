4f5746e1d3261793403ef7d95c388792f58c3028
Unknown <ioawnen@gmail.com>
2018-04-06 12:49:31 +0100

    Documenting

--- fileIO.py
+++ fileIO.py
@@ -1,15 +1,33 @@
 """This class is basically an interface for os library functions."""
 
-import os
 import logging
+import os
 import shutil
 
 
-def file_exists(path) -> bool:
+def file_exists(path: str) -> bool:
+    """Checks if a file exists
+    
+    Arguments:
+        path {str} -- Path of the file
+    
+    Returns:
+        bool -- File exists
+    """
+
     return os.path.exists(path)
 
 
 def move_file(target_dir, destination_dir, filename):
+    """Moves a file from the target directory to the destination directory.
+    Creates destination directory if it doesn't already exist.
+    
+    Arguments:
+        target_dir {[type]} -- Target directory
+        destination_dir {[type]} -- Destination directory
+        filename {[type]} -- Filename
+    """
+
     src = os.path.join(target_dir, filename)
     dst = os.path.join(destination_dir, filename)
 
@@ -25,8 +43,26 @@ def move_file(target_dir, destination_dir, filename):
 
 
 def change_file_permissions(target, mode):
+    """Changes the target file permissions to an input mode.
+    
+    Arguments:
+        target {[type]} -- Target file
+        mode {[type]} -- New permissions
+    """
+
     os.chmod(target, mode)
 
 
 def get_all_files(path, follow_symlinks=False):
+    """Returns all files / directories in an input path.
+    
+    Arguments:
+        path {str} -- Initial path
+    
+    Keyword Arguments:
+        follow_symlinks {bool} -- Should the search include symlinks (default: {False})
+    
+    Returns:
+        [type] -- See os.walk.
+    """
     return os.walk(path, followlinks=follow_symlinks)
--- logging.py
+++ logging.py
@@ -7,20 +7,39 @@ from settings import Settings
 colorama.init(autoreset=True)
 
 LOG_LEVELS = [
-    Fore.LIGHTRED_EX +      "ERROR  ",
-    Fore.LIGHTYELLOW_EX +   "WARNING",
-    Fore.LIGHTCYAN_EX +     "LOG    ",
-    Fore.LIGHTMAGENTA_EX +  "INFO   ",
-    Fore.LIGHTGREEN_EX +    "SILLY  "
+    Fore.LIGHTRED_EX        + "ERROR  ",
+    Fore.LIGHTYELLOW_EX     + "WARNING",
+    Fore.LIGHTCYAN_EX       + "LOG    ",
+    Fore.LIGHTMAGENTA_EX    + "INFO   ",
+    Fore.LIGHTGREEN_EX      + "SILLY  "
 ]
 
 
-def get_time():
+def get_time() -> str:
+    """Returns a formatted timestamp string
+    
+    Returns:
+        str -- Formatted timestamp string
+    """
+    
     ts = time.time()
     return datetime.datetime.fromtimestamp(ts).strftime('[%Y-%m-%d %H:%M:%S]')
 
 
-def out(str_in: str = "", log_level=2, override_level=False):
+def out(str_in: str = "", log_level=2, override_level=False) -> None:
+    """Outputs a formatted log of the input string to the console.
+
+    The output is printed in the following format:
+        [{timestamp}] {log level} - {string input}
+    Example: 
+        [2018-04-06 12:24:50] INFO  - This is an example log.
+    
+    Keyword Arguments:
+        str_in {str} -- String to output (default: {""})
+        log_level {int} -- Level to log against. See docs. (default: {2})
+        override_level {bool} -- Output log regardless of log_level setting. (default: {False})
+    """
+
     log_level_str = LOG_LEVELS[log_level]
     if override_level or Settings(False).get_settings().log_level >= log_level:
         print("{0}{1}{2} {3}{4} - {5}".format(Style.DIM, get_time(), Style.NORMAL, log_level_str, Fore.WHITE, str_in))
